eeef0b52169596a31536ae92970e316c6c2ac79b
rajat2654 <rajat97533@gmail.com>
2020-06-05 15:52:55 +0530

    self api

--- client/src/backend.js
+++ client/src/backend.js
@@ -1,2 +1,3 @@
-export const API = 'https://ecom-app-20.herokuapp.com/api'
-export const API1 = 'http://localhost:5000/api'
+export const API1 = 'https://ecom-app-20.herokuapp.com/api'
+export const API2 = 'http://localhost:5000/api'
+export const API = ''
