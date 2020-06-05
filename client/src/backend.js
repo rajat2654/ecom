8e9b0338aa460465176d3de3419d882b5f2a82f6
rajat2654 <rajat97533@gmail.com>
2020-06-05 15:58:43 +0530

    explicit conn

--- client/src/backend.js
+++ client/src/backend.js
@@ -1,3 +1,2 @@
-export const API1 = 'https://ecom-app-20.herokuapp.com/api'
-export const API2 = 'http://localhost:5000/api'
-export const API = ''
+export const API = 'https://ecom-app-20.herokuapp.com/api'
+export const API1 = 'http://localhost:5000/api'
